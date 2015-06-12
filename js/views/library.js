48d7da3dc15cdeff87130536024f73c5a3a0fd76
Emily Owaki <emily.owaki@gmail.com>
2015-06-11 21:48:19 -0500

    add book functionality works

--- js/views/library.js
+++ js/views/library.js
@@ -8,9 +8,27 @@ app.LibraryView = Backbone.View.extend({
 //takes an array of data and passes it to app.Library
   initialize: function(initialBooks) {
     this.collection = new app.Library(initialBooks);
+    //listens for the addBook event and updates if it happens
+    this.listenTo(this.collection, 'add', this.renderBook);
     this.render();
   },
 
+  events: {
+    'click #add' : 'addBook'
+  },
+
+  addBook: function(event) {
+    event.preventDefault();
+
+    var formInfo = {};
+    //gets the form inputs. If input isn't blank, it sets as value inside formInfo object
+    $('#addBook div').children('input').each(function(i, el) {
+      if($(el).val() !== ''){
+        formInfo[el.id] = $(el).val();
+      }
+    });
+    this.collection.add(new app.Book(formInfo));
+  },
   //render library by rendering each book in the collection
   render: function() {
     this.collection.each(function(item) {
